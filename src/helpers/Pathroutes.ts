2f90bc81a36102a5058d6e00e4873f571cc96555
VLDeMenezes <estudiodemenezes@gmail.com>
2024-08-21 09:21:44 -0300

    Pathroutes como helper

---
+++ src/helpers/Pathroutes.ts
@@ -0,0 +1,9 @@
+export const PATHROUTES = {
+  HOME: "/",
+  LOGIN: "/signin",
+  SIGNUP: "/signup",
+  USER_DASHBOARD: "/userDashboard",
+  VET_DASHBOARD: "/vetDashboard",
+  ADMIN_DASHBOARD: "/adminDashboard",
+  PET: "/pet",
+};
