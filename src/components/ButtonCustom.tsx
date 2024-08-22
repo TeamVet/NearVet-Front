b210913306fbaefb17e91d94c1d1cbc4c7f0a2e7
VLDeMenezes <estudiodemenezes@gmail.com>
2024-08-22 08:23:16 -0300

    Fix bg color of Button

--- src/components/ButtonCustom.tsx
+++ src/components/ButtonCustom.tsx
@@ -2,16 +2,16 @@ import { ButtonCustomOptionalProps, ButtonCustomProps } from "@/types/interfaces
 import Link from "next/link";
 
 
-const ButtonCustom: React.FC<ButtonCustomProps & Partial<ButtonCustomOptionalProps>> = ({ href, text, onClick, size = 'base', color = 'white', bgcolor = 'detail' }) => {
+const ButtonCustom: React.FC<ButtonCustomProps & Partial<ButtonCustomOptionalProps>> = ({ href, type = 'button', text, onClick, size = 'base', color = 'white', bgcolor = 'detail' }) => {
   const sizeClass = `text-${size}`;
   const colorClass = `text-${color}`;
   const bgColorClass = `bg-${bgcolor}`;
 
   return (
     href ? (
-      <Link href={href} aria-label={`Link para ${text}`} className={`px-5 py-2 m-auto rounded-lg text-lg ${sizeClass} ${colorClass} ${bgColorClass}`}>{text}</Link>
+      <Link href={href} aria-label={`Link para ${text}`} className={`px-5 py-2 m-auto rounded-lg  ${sizeClass} ${colorClass} ${bgColorClass}  `}>{text}</Link>
     ) : (
-      <button onClick={onClick} aria-label={`Boton para ${text}`} className={`px-5 py-2 m-auto rounded-lg text-lg ${sizeClass} ${colorClass} ${bgColorClass} `}>{text}</button>
+      <button type={type} onClick={onClick} aria-label={`Boton para ${text}`} className={`px-5 py-2 m-auto rounded-lg text-lg ${sizeClass} ${colorClass} ${bgColorClass} `}>{text}</button>
     )
   );
 };
