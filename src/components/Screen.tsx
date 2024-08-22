634f30b20db175bd7243af373fa6a9b3c9607603
VLDeMenezes <estudiodemenezes@gmail.com>
2024-08-22 18:47:22 -0300

    Screen without justify

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,8 +1,8 @@
+"use client"
 import { ScreenProps } from "@/types/interfaces";
-
-
 const Screen: React.FC<ScreenProps> = ({ children }) => {
-  return (<main className="shadow-lg w-full lg:w-5/6 flex flex-col mx-auto my-5 p-5 h-[100lvh] text-center justify-center rounded-md ">
+
+  return (<main className={`shadow-lg w-full lg:w-5/6 flex flex-col mx-auto my-5 p-5 h-[100lvh] text-center rounded-md dark:bg-darkBG`} >
     {children}
   </main>);
 };
