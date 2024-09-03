27ed224d0d9b614cb5dd8d95752df930eaa216ef
VLDeMenezes <estudiodemenezes@gmail.com>
2024-09-03 20:06:02 -0300

    retrotraer loading text

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,25 +1,15 @@
 "use client";
 
 import type {} from "ldrs";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 const Loading: React.FC = () => {
-  const [texto, setTexto] = useState<string | null>(null);
-  const [isVisible, setIsVisible] = useState(false);
-
   useEffect(() => {
     async function getLoader() {
       const { cardio } = await import("ldrs");
       cardio.register();
     }
     getLoader();
-
-    const timeoutId = setTimeout(() => {
-      setTexto("Está tardando mucho... pero seamos pacientes!");
-      setIsVisible(true);
-    }, 1000);
-
-    return () => clearTimeout(timeoutId);
   }, []);
 
   return (
@@ -28,17 +18,6 @@ const Loading: React.FC = () => {
         <div className="fixed mx-auto top-[30lvh]">
           <l-cardio size="100" stroke="4" speed="2" color="violet"></l-cardio>
         </div>
-        {texto && (
-          <div
-            className={`transition-opacity duration-1000 ${
-              isVisible ? "opacity-100" : "opacity-0"
-            } bg-white rounded px-5 py-2 m-2`}
-          >
-            <span className="text-detail md:text-3xl font-semibold">
-              {texto}
-            </span>
-          </div>
-        )}
       </div>
     </main>
   );
