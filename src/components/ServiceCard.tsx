21f311e088674aa1261e131d0c8c7c2c8900aa48
VLDeMenezes <estudiodemenezes@gmail.com>
2024-09-19 20:11:13 -0300

    Main changes

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,34 +1,46 @@
 import { Servicio } from "@/types/interfaces";
 import ButtonCustom from "./ButtonCustom";
 import PATHROUTES from "@/helpers/path-routes";
-export interface Service {
-  id: number;
-  logo: string;
+
+export interface TarjetaCategory {
+  id: string;
+  categoryService: string;
   description: string;
-  name: string;
+  image: string;
 }
-const VetCard: React.FC<Service> = (service) => {
+export interface TarjetaService {
+  id: string;
+  service: string;
+  description: string;
+  sendMesasge: string;
+  price: number;
+  durationMin: number;
+  veterinarianId: string;
+  categoryServiceId: string;
+  veterinarian: {
+    user: {
+      imgProfile: string;
+    };
+  };
+}
+export interface Service {
+  data: TarjetaService;
+}
+const VetCard: React.FC<Service> = (data) => {
+  if (!data) return;
+
   return (
-    <div className="flex flex-col bg-purpleBackground p-5 rounded-lg gap-5 md:w-[16em] lg:w-[20em] w-[12em] shadow-lg dark:bg-darkBackgroundFront">
-      <div className="flex justify-between">
-        <div className="flex items-center">
-          <img src={service.logo} alt="[serviceLogo]" />
-          <h1 className="text-sm md:text-lg lg:text-xl font-extrabold text-primary dark:text-white">
-            {service.name}
-          </h1>
+    <div className="flex flex-col bg-purpleBackground p-2 rounded-lg gap-2 md:w-[16em] lg:w-[20em] w-[12em] shadow-lg dark:bg-darkBackgroundFront hover:scale-105 cursor-default">
+      <div className="flex flex-col ">
+        <div className="flex flex-col items-center">
+          <h2 className="text-sm md:text-lg lg:text-xl font-extrabold text-purpleTitles my-1">
+            {data.data.service}
+          </h2>
         </div>
       </div>
       <p className="text-justify dark:text-gray-300 text-sm md:text-base lg:text-lg">
-        asdasd
-        {service.description}
+        {data.data.description}
       </p>
-      <div className="flex justify-end">
-        <ButtonCustom
-          href={PATHROUTES.NEWAPPOINTMEN}
-          text="Agenda Ahora"
-          className="rounded-lg text-white bg-detail p-2 shadow-md hover:scale-105"
-        />
-      </div>
     </div>
   );
 };
